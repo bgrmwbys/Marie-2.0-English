a8d0d667389c4b29e71b1a3da93438c6e8ce08c0
bgrmwbys <77823759+bgrmwbys@users.noreply.github.com>
2021-01-22 01:22:45 -0500

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -17,9 +17,9 @@ from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
-PM_START_TEXT = """
-היי ברוך הבא ל {} 
-בוט זה רץ ע"י  [this person](tg://user?id={}).
+PM_START_TEXT =
+"""היי {} ברוך הבא ל {} ! 
+הבוט רץ ע"י [Yishaicohen](tg://user?id={})
 """
 
 HELP_STRINGS = """
